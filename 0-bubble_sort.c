1cb1923432eaf8f5ac0fde0b16117f57744b48d6
1819@holbertonschool.com <1819@holbertonschool.com>
2020-10-18 14:45:52 -0500

    adding files for task zero

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function that runs the bubble sort method
+ *
+ * @array: input array
+ * @size: linked list structure
+ *
+ * Return: no return
+ */
+
+void bubble_sort(int *array, size_t size)
+
+{
+	size_t stepper;
+	size_t i;
+	int temp;
+
+	if (array == NULL)
+	{
+		return;
+	}
+
+	for (stepper = 0; stepper < size -1; stepper++)
+	{
+		for (i = 0; i < size - stepper - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
+
