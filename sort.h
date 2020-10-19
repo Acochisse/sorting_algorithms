2fd0702d32b73fb901e9b79cdc684055873e8033
1819@holbertonschool.com <1819@holbertonschool.com>
2020-10-19 09:30:11 -0500

    added swap function

--- sort.h
+++ sort.h
@@ -34,5 +34,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void swap(int *first, int *second);
 
 #endif
