3e80f10cb607aacc34856ddad56625fa95109f9f
1819@holbertonschool.com <1819@holbertonschool.com>
2020-10-19 09:29:48 -0500

    created files for task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,24 +4,40 @@
  * selection_sort - function that runs the selection sort algorithm
  * @array: input array
  * @size: doubly linked list
- * 
+ *
  * Return: no return
  */
 
 void selection_sort(int *array, size_t size)
 {
-    size_t *temp, *current;
 
-       int i, j, min_idx; 
-  
-    for (i = 0; i < n-1; i++) 
-    { 
-        
-        min_idx = i; 
-        for (j = i+1; j < n; j++) 
-              if (arr[j] < arr[min_idx]) 
-                    min_idx = j; 
-  
-         
-    } 
+	unsigned long int i, j, min_idx;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		swap(&array[min_idx], &array[i]);
+		print_array(array, size);
+	}
+}
+
+
+/**
+ * swap - swaps two int positions in an array
+ * @first: first position
+ * @second: second position
+ * Return: void
+ */
+void swap(int *first, int *second)
+{
+	int temp;
+
+	temp = *first;
+	*first = *second;
+	*second = temp;
 }
