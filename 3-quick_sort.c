b309997c96efb76ffcd114ad14bcf51c15680854
1819@holbertonschool.com <1819@holbertonschool.com>
2020-10-20 07:22:36 -0500

    update to styling

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,14 +1,84 @@
 #include "sort.h"
 
 /**
- * quick_sort - function that runs sort by the method of quicksort
- * @array: input array
- * @size: doubly linked list
- * 
- * 
+ * partition - finds the partition for quicksort using Lomuto
+ * @array: array to sort
+ * @low: lowest partition to sort
+ * @high: highest partition to sort
+ * @size: size of the array
+ *
+ * Return: index
+ */
+
+size_t partition(int *array, ssize_t low, ssize_t high, size_t size)
+{
+	ssize_t i, j;
+	int swap, pivot;
+
+	pivot = array[high];
+	i = low - 1;
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap = array[i];
+				array[i] = array[j];
+				array[j] = swap;
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[high] < array[i + 1])
+	{
+		swap = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = swap;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+
+/**
+ * quicksort - sorts a partition of an array of integers
+ * @array: array to sort
+ * @low: lowest index of the partition to sort
+ * @high: highest index of the partition to sort
+ * @size: size of the array
+ *
+ * Return: no return
+ */
+
+void quicksort(int *array, ssize_t low, ssize_t high, size_t size)
+
+{
+	ssize_t pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, low, high, size);
+		quicksort(array, low, pivot - 1, size);
+		quicksort(array, pivot + 1, high, size);
+
+	}
+}
+
+
+/**
+ * quick_sort - sorts an array of integers in ascending order using the
+ * Quick sort algorithm
+ * @array: The array to sort
+ * @size: The size of the array
+ *
+ * Return: no return
  */
 void quick_sort(int *array, size_t size)
 {
-    size_t *current, *temp;
-    
+	if (array == NULL || size < 2)
+		return;
+
+	quicksort(array, 0, size - 1, size);
 }
